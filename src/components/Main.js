9e0f8a87d9f9109b03ea706b87282234ce5cf8fc
CraigYeoman <yeomancraig@gmail.com>
2022-09-27 06:41:47 -0500

    Adding facebook page plugin

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,7 @@ const Main = () => {
             <div className="video--container">
                 <video className="background--video" src={video} alt="video" autoPlay muted loop disablePictureInPicture playsInline controlsList="nofullscreen nodownload"/>
             </div >
+            <div className="content--row">
                 <div className="content--container">
                     <div className="info--container">
                         <h3>Custom Engines</h3>
@@ -19,6 +20,8 @@ const Main = () => {
                     </div>
                 </div>
                 <Champions />
+                <div class="fb-page" data-href="https://www.facebook.com/yeomanraceengines/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/yeomanraceengines/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/yeomanraceengines/">Yeoman Race Engines</a></blockquote></div>
+            </div>
         </div>
     )
 }
