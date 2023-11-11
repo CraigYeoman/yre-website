1ed6ab4d6080b4cfc36e93133e71f915033a1229
CraigYeoman <yeomancraig@gmail.com>
2023-11-11 11:19:57 -0600

    Update Index, drag race, street pages

--- src/components/Engines/DragRace.js
+++ src/components/Engines/DragRace.js
@@ -1,9 +1,26 @@
+import dragRace1 from "../../images/dragrace1.jpg"
+import dragRace2 from "../../images/dragrace2.jpg"
+import dragRace3 from "../../images/dragrace3.jpg"
+import dragRace4 from "../../images/dragrace4.jpg"
+import dragRace5 from "../../images/dragrace5.jpg"
+import dragRace6 from "../../images/dragrace6.jpg"
+
 const DragRace = () => {
     return (
         <div className="engine--container">
-            <h1 className="engine-h1">Drag Racing</h1>
-                <h3>The majority of drag racing classes offer a wide variety of engine combinations. With that being said, prices vary greatly depending on your application. Whether you're looking for all motor, nitrous, blower, or turbos, we can customize a motor to suit your needs.</h3>
+        <h1 className="engine-h1">Drag Racing</h1>
+        <h3>
+        The majority of drag racing classes offer a wide variety of engine combinations. With that being said, prices vary greatly depending on your application. Whether you're looking for all motor, nitrous, blower, or turbos, we can customize a motor to suit your needs.
+        </h3>
+        <div className="img-gallery">
+          <img src={dragRace1} alt="car" />
+          <img src={dragRace2} alt="car" />
+          <img src={dragRace3} alt="car" />
+          <img src={dragRace4} alt="car" />
+          <img src={dragRace5} alt="car" />
+          <img src={dragRace6} alt="car" />
         </div>
+      </div>
 
     )
 }
--- src/components/Engines/Street.js
+++ src/components/Engines/Street.js
@@ -1,3 +1,10 @@
+import car1 from "../../images/car1.jpg"
+import car2 from "../../images/car2.jpg"
+import car3 from "../../images/car3.jpg"
+import engine1 from "../../images/engine1.jpg"
+import engine2 from "../../images/engine2.jpg"
+import engine3 from "../../images/engine3.jpg"
+
 const Street = () => {
   return (
     <div className="engine--container">
@@ -6,6 +13,14 @@ const Street = () => {
         We can build your street motor from a stock small block to a blower big
         block. Pricing varies depending on your goals.
       </h3>
+      <div className="img-gallery">
+        <img src={car1} alt="car" />
+        <img src={car2} alt="car" />
+        <img src={car3} alt="car" />
+        <img src={engine1} alt="car" />
+        <img src={engine2} alt="car" />
+        <img src={engine3} alt="car" />
+      </div>
     </div>
   );
 };
