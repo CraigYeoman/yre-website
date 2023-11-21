b4835e4ef4f029e086a8a3756866fde3cefb4b27
CraigYeoman <yeomancraig@gmail.com>
2023-11-21 15:41:06 -0600

    New navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,65 +3,44 @@ import { Link } from "react-router-dom";
 import "../style.css";
 import logo from "../images/yeomanRaceEnginesLogo.svg";
 import { TiThMenu } from "react-icons/ti";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
-  const [isNavExpanded, setNavExpanded] = useState(false);
 
-  const toggleMenu = () => {
-    setNavExpanded(!isNavExpanded);
-  };
+const Header = () => {
+  const [color, setColor] = useState(false);
+
+  const changeColor = () => {
+    if (window.scrollY >= 90) {
+      setColor(true)
+    } else {
+      setColor(false)
+    }
+  } 
+
+  window.addEventListener('scroll', changeColor)
 
   return (
-    <nav className="nav--container">
-      <div className="nav--bar">
-        <Link to="/" className="nav--icon">
-          <img src={logo} className="nav--icon" alt="logo" />
-        </Link>
-
-        <Link to="/" className="nav--item">
-          Home
-        </Link>
-        <Link to="/Engines" className="nav--item">
-          Engines
-        </Link>
-        <Link to="/Contact" className="nav--item">
-          Contact
-        </Link>
-
-        <button
-          className="nav--button"
-          // onMouseOver={() => setNavExpanded(true)}
-          // onMouseLeave={() => setNavExpanded(false)}
-          // onMouseEnter={() => setNavExpanded(true)}
-          // onClick={() => setNavExpanded(!isNavExpanded)}
-          onClick={toggleMenu}
-        >
-          <TiThMenu />
-        </button>
-      </div>
-      <div className="nav--bar title">
-        <div className="nav--item">200+ Wins in 2022</div>
-      </div>
-
-      <div
-        className={
-          isNavExpanded ? "navigation--menu expanded" : "navigation--menu"
-        }
-      >
-        <div className="nav--dropdown">
-          <div className="nav--link">
-            <Link to="/">Home</Link>
-          </div>
-          <div className="nav--link">
-            <Link to="/Engines">Engines</Link>
-          </div>
-          <div className="nav--link">
-            <Link to="/Contact">Contact</Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" fixed="top" className={color ? 'header header-bg': "header"}>
+      <Container>
+        <Navbar.Brand> 
+          <Link to="/" >
+            <img src={logo} className="nav--icon" alt="logo" />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Engines">Engines</Nav.Link>
+            <Nav.Link href="/Contact">Contact</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Header;
