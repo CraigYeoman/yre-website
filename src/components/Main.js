0f08e9b543e47e2bd21e4d9a12bf485384fce15d
CraigYeoman <yeomancraig@gmail.com>
2023-11-22 12:58:23 -0600

    Switching to boostrap.Revise of index page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from "react"
 import video from "../images/video.mp4"
-import "../style.css"
 import Champions from "./Champions"
 
 const Main = () => {
@@ -10,7 +9,7 @@ const Main = () => {
             <div className="video--container">
                 <video className="background--video" src={video} alt="video" autoPlay muted loop disablePictureInPicture playsInline controlsList="nofullscreen nodownload"/>
             </div >
-            <div className="content--row">
+            {/* <div className="content--row">
                 <div className="content--container">
                     <div className="info--container">
                         <h3>Custom Engines</h3>
@@ -27,6 +26,42 @@ const Main = () => {
                 <div className="fb">
                     <div className="fb--container">
                         <div className="fb-page" data-href="https://www.facebook.com/yeomanraceengines/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/yeomanraceengines/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/yeomanraceengines/">Yeoman Race Engines</a></blockquote></div>
+                    </div>
+                </div>
+            </div> */}
+            <div className="b-example-divider"></div>
+
+            <div className="container col-xxl-8 px-4 py-5">
+                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                    <div className="col-10 col-sm-8 col-lg-6">
+                    <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+                    </div>
+                    <div className="col-lg-6">
+                    <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Your way to victory lane</h1>
+                    <p className="lead text-secondary">A winning racing programs requires a lot.  Let us take care of your engine program.</p>
+                    <p className="lead text-secondary">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <p className="lead text-secondary">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button type="button" className="btn btn-outline-secondary btn-lg px-4">About</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="b-example-divider"></div>
+
+            <div className="container col-xxl-8 px-4 py-5">
+                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                    <div className="col-lg-6">
+                        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                        <p className="lead text-secondary">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+                        </div>
+                    </div>
+                    <div className="col-10 col-sm-8 col-lg-6">
+                        <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
                     </div>
                 </div>
             </div>
